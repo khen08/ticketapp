ed50383948f4e1ba900af655b281510f3baa0353
Louiskhen Yagdulas <yagdulasl@gmail.com>
2024-08-01 16:50:43 +0800

    Structure Complete

---
+++ app/layout.tsx
@@ -0,0 +1,32 @@
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import "./globals.css";
+import MainNav from "@/components/MainNav";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+  title: "Ticket Application",
+  description: "Developed by Louiskhen Yagdulas",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body className={inter.className}>
+        <nav className="flex flex-col items-center border-b mb-5 px-5 py-3">
+          <div className="max-w-6xl w-full">
+            <MainNav></MainNav>
+          </div>
+        </nav>
+        <main className="flex flex-col items-center">
+          <div className="max-w-6xl w-full">{children}</div>
+        </main>
+      </body>
+    </html>
+  );
+}
