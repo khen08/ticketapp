7ae86b5c8e911d336dab8bcf9239095e2ea121a0
Louiskhen Yagdulas <yagdulasl@gmail.com>
2024-08-03 16:37:24 +0800

    established db connection and added ticket form

--- app/tickets/page.tsx
+++ app/tickets/page.tsx
@@ -1,7 +1,23 @@
 import React from "react";
+import prisma from "@/prisma/db";
+import DataTable from "./DataTable";
+import Link from "next/link";
+import { buttonVariants } from "@/components/ui/button";
 
-const Tickets = () => {
-  return <div>Tickets</div>;
+const Tickets = async () => {
+  const tickets = await prisma.ticket.findMany();
+
+  return (
+    <div>
+      <Link
+        href="/tickets/new"
+        className={buttonVariants({ variant: "default" })}
+      >
+        New Ticket
+      </Link>
+      <DataTable tickets={tickets} />
+    </div>
+  );
 };
 
 export default Tickets;
---
+++ components/ToggleMode.tsx
@@ -0,0 +1,37 @@
+"use client";
+
+import { useState, useEffect } from "react";
+import { useTheme } from "next-themes";
+import { Sun, Moon } from "lucide-react";
+import { Button } from "./ui/button";
+
+const ToggleMode = () => {
+  const { theme, setTheme } = useTheme();
+  const [mounted, setMounted] = useState(false);
+
+  useEffect(() => {
+    setMounted(true);
+  }, []);
+
+  if (!mounted) {
+    return <Button variant="outline" size="icon" disabled={true}></Button>;
+  }
+
+  const light = theme === "light";
+
+  return (
+    <Button
+      variant="outline"
+      size="icon"
+      onClick={() => setTheme(`${light ? "dark" : "light"}`)}
+    >
+      {light ? (
+        <Moon className="hover:cursor-pointer hover:text-primary" />
+      ) : (
+        <Sun className="hover:cursor-pointer hover:text-primary" />
+      )}
+    </Button>
+  );
+};
+
+export default ToggleMode;
