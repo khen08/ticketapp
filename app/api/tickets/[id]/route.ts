d068fcb56fdbe5e453d7806acac1dd7820f9b411
Louiskhen Yagdulas <yagdulasl@gmail.com>
2024-08-04 14:21:22 +0800

    protected api endpoints

--- app/api/tickets/[id]/route.ts
+++ app/api/tickets/[id]/route.ts
@@ -1,12 +1,27 @@
 import prisma from "@/prisma/db";
 import { ticketPatchSchema } from "@/ValidationSchemas/ticket";
+import { getServerSession } from "next-auth";
 import { NextRequest, NextResponse } from "next/server";
+import options from "../../auth/[...nextauth]/options";
 
 interface Props {
   params: { id: string };
 }
 
 export async function PATCH(request: NextRequest, { params }: Props) {
+  const session = await getServerSession(options);
+
+  if (!session) {
+    return NextResponse.json({ error: "Not Authenticated" }, { status: 401 });
+  }
+
+  if (session.user.role !== "ADMIN") {
+    return NextResponse.json(
+      { error: "Not Authenticated as Admin" },
+      { status: 401 }
+    );
+  }
+
   const body = await request.json();
   const validation = ticketPatchSchema.safeParse(body);
 
@@ -37,6 +52,19 @@ export async function PATCH(request: NextRequest, { params }: Props) {
 }
 
 export async function DELETE(request: NextRequest, { params }: Props) {
+  const session = await getServerSession(options);
+
+  if (!session) {
+    return NextResponse.json({ error: "Not Authenticated" }, { status: 401 });
+  }
+
+  if (session.user.role !== "ADMIN") {
+    return NextResponse.json(
+      { error: "Not Authenticated as Admin" },
+      { status: 401 }
+    );
+  }
+
   const ticket = await prisma.ticket.findUnique({
     where: { id: parseInt(params.id) },
   });
--- app/api/users/route.ts
+++ app/api/users/route.ts
@@ -2,8 +2,23 @@ import prisma from "@/prisma/db";
 import { userSchema } from "@/ValidationSchemas/users";
 import { NextRequest, NextResponse } from "next/server";
 import bcrypt from "bcryptjs";
+import { getServerSession } from "next-auth";
+import options from "../auth/[...nextauth]/options";
 
 export async function POST(request: NextRequest) {
+  const session = await getServerSession(options);
+
+  if (!session) {
+    return NextResponse.json({ error: "Not Authenticated" }, { status: 401 });
+  }
+
+  if (session.user.role !== "ADMIN") {
+    return NextResponse.json(
+      { error: "Not Authenticated as Admin" },
+      { status: 401 }
+    );
+  }
+
   const body = await request.json();
   const validation = userSchema.safeParse(body);
 
